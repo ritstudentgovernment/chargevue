3829f1e0de60dea47bf3b1216804734c152aa256
gabelandau <gabelandau14@gmail.com>
2017-09-13 13:50:56 -0400

    Added page title to committee page

--- src/pages/Committee.vue
+++ src/pages/Committee.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="dashboard">
     <Header />
-    <p>Page name: {{ this.committee.friendlyname }}</p>
+    <CommitteesMenu />
+    <div class="pagename">
+      <h1>{{ this.committee.friendlyname }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import CommitteeInfo from '../mixins/committeeinfo'
 import Header from '../components/Header'
+import CommitteesMenu from '../components/CommitteesMenu'
 
 export default {
   name: 'dashboard',
   mixins: [CommitteeInfo],
   components: {
-    'Header': Header
+    'Header': Header,
+    'CommitteesMenu': CommitteesMenu
   },
   data () {
     return {
@@ -24,5 +29,15 @@ export default {
 </script>
 
 <style scoped>
+.pagename {
+  background: #000;
+}
 
+.pagename h1 {
+  margin: 0;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 75px 0;
+  color: #fff;
+}
 </style>
