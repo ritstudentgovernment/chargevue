6694f57da0525eff93ce421d565a0f59a8430b6a
gabelandau <gabelandau14@gmail.com>
2017-09-18 14:58:11 -0400

    Added charge page

---
+++ src/pages/Charge.vue
@@ -0,0 +1,35 @@
+<!--
+
+filename: Charge.vue
+description: Page for an indiviudual charge
+
+author: Gabe Landau <gll1872@rit.edu>
+
+-->
+
+<template>
+  <div>
+    <Header />
+    <CommitteesMenu />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import CommitteesMenu from '../components/CommitteesMenu'
+
+export default {
+  name: 'dashboard',
+  components: {
+    'Header': Header,
+    'CommitteesMenu': CommitteesMenu
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
