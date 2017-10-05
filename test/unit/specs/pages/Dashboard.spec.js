a19e9e539423f5f5597be97a010f5653209ca50c
gabelandau <gabelandau14@gmail.com>
2017-10-05 17:18:23 -0400

    Imported router into dashboard test

--- test/unit/specs/pages/Dashboard.spec.js
+++ test/unit/specs/pages/Dashboard.spec.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Dashboard from '@/pages/Dashboard'
+import router from '@/router/index'
 
 describe('Dashboard.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Dashboard)
-    const vm = new Constructor().$mount()
+    const vm = new Constructor({router}).$mount()
     expect(vm.$el.querySelector('#dashboard_title').textContent)
       .to.equal('Projects In Progress')
   })
