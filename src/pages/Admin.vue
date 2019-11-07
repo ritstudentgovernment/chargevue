51974296095cfdc4fadbf384f146b154025393d3
Omar De La Hoz <omar.dlhz@hotmail.com>
2019-11-07 14:54:55 -0500

    Removed unused variables

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -276,7 +276,6 @@ export default {
       createDescription: null,
       createLocation: null,
       createCommitteeHead: null,
-      createMeetingCommittee: null,
       createMeetingDay: null,
       createMeetingAmPm: null,
       createMeetingHour: null,
@@ -288,7 +287,6 @@ export default {
       editTitle: null,
       editDescription: null,
       editLocation: null,
-      editMeetingCommittee: null,
       editMeetingDay: null,
       editMeetingHour: null,
       editMeetingMinute: null,
@@ -335,7 +333,6 @@ export default {
           token: this.getToken(),
           title: this.createTitle,
           description: this.createDescription,
-          committee: this.createMeetingCommittee,
           location: this.createLocation,
           meeting_time: time,
           meeting_day: day,
@@ -359,7 +356,6 @@ export default {
       this.createDescription = null
       this.createLocation = null
       this.createCommitteeHead = null
-      this.createMeetingCommittee = null
       this.createMeetingDay = null
       this.createMeetingAmPm = null
       this.createMeetingHour = null
@@ -380,7 +376,6 @@ export default {
           token: this.getToken(),
           id: this.editTitle.toLowerCase(),
           description: this.editDescription,
-          meeting_committee: this.editMeetingCommittee,
           location: this.editLocation,
           meeting_time: time,
           meeting_day: day,
@@ -392,7 +387,6 @@ export default {
           token: this.getToken(),
           id: this.editTitle.toLowerCase().replace(/\s/g, ''),
           description: this.editDescription,
-          meeting_committee: this.editMeetingCommittee,
           location: this.editLocation,
           meeting_time: time,
           meeting_day: day,
@@ -456,7 +450,6 @@ export default {
       this.editMeetingAmPm = dateTimeObj.ampm
       this.editTitle = data.title
       this.editDescription = data.description
-      this.editMeetingCommittee = data.meetingCommittee
       this.editLocation = data.location
       this.editCommitteeHead = data.head
     },
