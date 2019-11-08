6079bd3ce90837009bd740d4c965e6bc27f4ede5
Moisés Lora <malexanderp@gmail.com>
2019-11-08 16:15:26 -0500

    Add way to distinguish committee head (FE) (#28)
    * added distinguishing
    
    * added label. missing alignment
    
    * working css
    
    * working css fixes
    
    * Removed extra css

--- src/components/CommitteeMembers.vue
+++ src/components/CommitteeMembers.vue
@@ -10,8 +10,13 @@
     <div class="title">Committee Members</div>
     <div class="divider"></div>
     <div class="content">
+      <div class="head" :key="committee.head">
+          <span class="head-label">HEAD</span>
+          <span class="head-container">{{ committee.head }}</span>
+      </div>
       <div class="member" v-for="member in members" :key="member.id">
         <span>{{ member.id }}</span>
+
       </div>
     </div>
   </div>
@@ -23,7 +28,10 @@ export default {
   data () {
     return {
       members: null,
-      committee: { 'description': 'committee' }
+      committee: {
+        'description': 'committee',
+        'head': ''
+      }
     }
   },
   sockets: {
@@ -82,6 +90,38 @@ export default {
     display: inline-block;
     width: 80%;
     text-align: center;
+  }
+
+  .head {
+    color: #fff;
+    font-size: 14pt;
+    font-weight: 300;
+    display: inline-block;
+    width: 24%;
+  }
+  .head-container {
+    background-color: #f36e21;
+    display: inline-block;
+    padding: 10px 45px 10px 45px;
+    margin: 10px 0 10px 0;
+    text-align: center;
+  }
+
+  .head-label {
+    float: left;
+    -webkit-writing-mode: vertical-rl;
+    -ms-writing-mode: tb-rl;
+    writing-mode: vertical-rl;
+    background-color: #000;
+    color: white;
+    font-size: 0.7rem;
+    letter-spacing: 0.05rem;
+    padding: 0.2rem;
+    margin: 10px 0 10px 10px;
+  }
+
+  .head-content {
+    width: 100%;
   }
 
   .content {
