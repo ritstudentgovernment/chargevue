c7554d450c5024c13316df13b1e7e39a8675a88b
Eli Parrish (RIT Student) <ep5756@sg-700.main.ad.rit.edu>
2020-02-10 16:43:18 -0500

    Add member modal works, committee members display has bugs

--- src/components/CommitteeMembers.vue
+++ src/components/CommitteeMembers.vue
@@ -9,19 +9,22 @@
   <div class="committee_members">
     <div class="title">Committee Members</div>
     <div class="divider"></div>
-    <div class="content">
-      <div class="head">
-          <span class="head-label">HEAD</span>
-          <span class="head-container">{{ committee.head }}</span>
-      </div>
-      <div class="member" v-for="member in members" :key="member.id">
-        <span>{{ member.id }}</span>
 
+    <div class="member-labels">
+      
+      <div class="head">
+        <span class="head-label">HEAD</span>
+        <span class="head-container">{{ committeeHead}}</span>
+     </div>
+
+      <div class="member">
+        <div v-for="member in members" :key="member.id">
+          <span v-if="member.id != committeeHead" class="member-container">{{ member.id }}</span>
+        </div>
       </div>
+
     </div>
   </div>
-
-  
 </template>
 
 <script>
@@ -33,15 +36,18 @@ export default {
       committee: {
         'description': 'committee',
         'head': ''
-      }
+      },
+      committeeHead: null
     }
   },
   sockets: {
     get_committee: function (data) {
       this.committee = data
+      this.committeeHead = this.committee.head
     },
     get_members: function (data) {
       this.members = data.members
+      console.log(this.members)
     }
   },
   beforeMount () {
@@ -76,16 +82,8 @@ export default {
   .divider {
     border-top: 1px solid #000;
   }
-
-  .membersbox {
-    color: #fff;
-    font-size: 14pt;
-    font-weight: 300;
-    display: inline-block;
-    padding: 5px;
-  }
   
-  .members-container {
+  .member-container {
     background-color: #f36e21;
     display: inline-block;
     padding: 10px 45px 10px 45px;
@@ -93,29 +91,17 @@ export default {
     text-align: center;
   }
 
-  .members-label {
-    float: left;
-    -webkit-writing-mode: vertical-rl;
-    -ms-writing-mode: tb-rl;
-    writing-mode: vertical-rl;
-    background-color: #000;
-    color: white;
-    font-size: 0.7rem;
-    letter-spacing: 0.05rem;
-    padding: 0.2rem;
-    margin: 10px 0 10px 10px;
-  }
-
-  .content {
-    padding: 10px;
-  }
-
   .member {
     color: #fff;
     font-size: 14pt;
     font-weight: 300;
     display: inline-block;
-    width: 25%;
+  }
+
+  .member-lables {
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
   }
 
   .head {
