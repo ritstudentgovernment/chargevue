b68af243999593c3b42d37d481cd55ce8f3ddedf
blackinferno1000 <seanalexander360@gmail.com>
2020-02-25 16:07:18 -0500

    computed property created

--- src/components/CommitteesMenu.vue
+++ src/components/CommitteesMenu.vue
@@ -13,7 +13,7 @@ author: Gabe Landau <gll1872@rit.edu>
       <nav class="navbar" role="navigation" aria-label="committee navigation">
         <div class="navbar-menu is-active">
           <div class="navbar-start"></div>
-          <a class='navbar-item' v-if="item.enabled" is-hoverable v-for="(item, index) in committees"><button @click="redirect(item.id)">{{item.title}}</button></a>
+          <a class='navbar-item' v-if="item.enabled" is-hoverable v-bind:key="index" v-for="(item, index) in committees"><button @click="redirect(item.id)">{{item.title}}</button></a>
           <div class="navbar-end"></div>
         </div>
       </nav> 
@@ -42,6 +42,13 @@ export default {
   methods: {
     redirect (itemId) {
       this.$router.push('/committee/' + itemId)
+    }
+  },
+  computed: {
+    enabled: function (item) {
+      if (item.enabled) {
+        return item
+      }
     }
   }
 }
