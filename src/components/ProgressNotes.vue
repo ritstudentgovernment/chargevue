a2593b897d8f962f893ca45efe166b61373605ef
Eli Parrish (RIT Student) <ep5756@sg-700.main.ad.rit.edu>
2020-03-04 15:18:31 -0500

    saving

--- src/components/ProgressNotes.vue
+++ src/components/ProgressNotes.vue
@@ -9,19 +9,43 @@ author: Gabe Landau <gll1872@rit.edu>
 
 <template>
   <div class="description">
+
+      <div class="modal" v-bind:class="{ 'is-active': showNewNoteModal }">
+        <div class="modal-background" v-on:click="closeModals()"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Add new progress note</p>
+          </header>
+          <section class="modal-card-body">
+
+            <div class="field">
+              <label class="label">Note</label>
+              <div class="control">
+                <input class="input" type="text" v-model="newNote">
+              </div>
+            </div>
+
+          </section>
+          <footer class="modal-card-foot">
+            <button class="button is-primary" v-on:click="addNote()">Save note</button>
+            <button class="button" v-on:click="closeModals()">Cancel</button>
+          </footer>
+        </div>
+    </div>
+
     <div class="columns">
       <div class="column">
         <div class="description_title">Charge progress notes</div>
       </div>
       <div class="column">
-        <button class="button is-primary">Add Note</button>
+        <button class="button is-primary" @click="openAddModal()">Add Note</button>
       </div>
     </div>
     <div class="divider"></div>
     <div class="rows">
         <li v-for="value in charge.progress_notes" v-bind:key = value>
             {{ value }}
-  </li>
+        </li>
     </div>
   </div>
 </template>
@@ -33,6 +57,55 @@ export default {
   components: {},
   data () {
     return {
+      localCharge: {},
+      newNote: '',
+      showNewNoteModal: false,
+      showEditNoteModal: false,
+      showDeleteNoteModal: false,
+      editChargeResponse: {
+        show: false,
+        message: null,
+        success: null
+      }
+    }
+  },
+  methods: {
+    addNote () {
+      console.log(this.newNote)
+      console.log(this.charge.progress_notes.length)
+    },
+    openAddModal () {
+      console.log('gottem')
+      this.showNewNoteModal = true
+    },
+    closeModals () {
+      this.showNewNoteModal = false
+      this.showEditNoteModal = false
+      this.showDeleteNoteModal = false
+    },
+    handleChargeEdits (data) {
+      if (data.success) {
+        this.editChargeResponse.show = true
+        this.editChargeResponse.success = true
+        this.editChargeResponse.message = data.success
+        var that = this
+        setTimeout(function () {
+          that.closeModals()
+        }, 2000)
+      } else if (data.error) {
+        this.editChargeResponse.show = true
+        this.editChargeResponse.success = false
+        this.editChargeResponse.message = data.error
+      }
+    }
+  },
+  sockets: {
+    edit_charge: function (data) {
+      this.handleChargeEdits(data)
+      // Only update the prop if the edit was successful
+      if (data.success) {
+        this.$emit('updateCharge', this.localCharge)
+      }
     }
   }
 }
@@ -73,6 +146,10 @@ export default {
 
   .button {
       float: right;
+  }
+
+  .field {
+      padding-right: 20px;
   }
 
 </style>
--- src/pages/Charge.vue
+++ src/pages/Charge.vue
@@ -60,7 +60,6 @@ export default {
   },
   sockets: {
     get_charge: function (data) {
-      console.log(data)
       this.charge = data
       this.charge.created_at = this.charge.created_at.substring(5, 7) + '/' + this.charge.created_at.substring(8, 10) + '/' + this.charge.created_at.substring(0, 4)
     },
